744a48ca31c6d854fc87a2c5312aac74b43de415
Hanna Lychauka <zuzga@tut.by>
2019-10-12 11:45:40 +0300

    class WriteFile

--- task_1_rooms_students.py
+++ task_1_rooms_students.py
@@ -1,5 +1,6 @@
 import json
 
+
 class OpenFile():
     """
     Open file with specified format, save file data to variable and return the variable
@@ -10,3 +11,14 @@ class OpenFile():
         with open('{}.{}'.format(file_name, file_format), 'r') as f:
             text_file = json.load(f)
         return text_file
+
+
+class WriteFile():
+    """
+    Write data to file with specified format
+    """
+
+    @staticmethod
+    def write_to_file(file_name, file_format, text):
+        with open('{}.{}'.format(file_name, file_format), 'w') as f:
+            json.dump(text, f)
