feb928daf3f9870637b22e60d28bcdccb7c4f272
Hanna Lychauka <zuzga@tut.by>
2019-10-12 11:54:40 +0300

    class JoinRoomsAndStudents

--- task_1_rooms_students.py
+++ task_1_rooms_students.py
@@ -22,3 +22,31 @@ class WriteFile():
     def write_to_file(file_name, file_format, text):
         with open('{}.{}'.format(file_name, file_format), 'w') as f:
             json.dump(text, f)
+
+
+class JoinRoomsAndStudents():
+    """
+    Append data of one list with data of another list
+    """
+
+    @staticmethod
+    def make_student_dict(students_list):
+        """
+        Returns dictionary, made of specified list
+        """
+        student_dict = {}
+
+        for i in students_list:
+            student_dict.setdefault(str(i['room']), []).append(i)
+
+        return student_dict
+
+    @staticmethod
+    def join_students_to_rooms(rooms_list, student_dict):
+        """
+        Returns list with appended data of dictionary
+        """
+        for i in rooms_list:
+            i['students'] = student_dict.get(str(i['id']))
+
+        return rooms_list
